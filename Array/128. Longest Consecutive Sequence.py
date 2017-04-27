65610136ae7da172ee697334cf16276f19488c35
xilixjd <349729220@qq.com>
2017-04-27 11:04:20 +0800

    longest consecutive sequence solve

--- Array/128. Longest Consecutive Sequence.py
+++ Array/128. Longest Consecutive Sequence.py
@@ -14,6 +14,7 @@ Your algorithm should run in O(n) complexity.
 class Solution(object):
     def longestConsecutive(self, nums):
         """
+        未通过 64 / 68 test cases passed.
         :type nums: List[int]
         :rtype: int
         """
@@ -22,21 +23,54 @@ class Solution(object):
             if nums_dict.get(n) is None:
                 nums_dict[n] = set([n])
         for key in nums_dict:
-            print key
             if key + 1 in nums_dict:
                 nums_dict[key + 1] |= nums_dict[key]
                 nums_dict[key] |= nums_dict[key + 1]
             if key - 1 in nums_dict:
                 nums_dict[key - 1] |= nums_dict[key]
                 nums_dict[key] |= nums_dict[key - 1]
-        max_len = 0
+        max_len = { 'length': 0}
         for key in nums_dict:
-            max_len = max(max_len, len(nums_dict[key]))
+            if max_len['length'] < len(nums_dict[key]):
+                max_len['length'] = len(nums_dict[key])
+                max_len['key'] = key
+        max_set = set()
+        for k in nums_dict[max_len['key']]:
+            max_set |= nums_dict[k]
 
         # print nums_dict
-        print max_len
-        return max_len
+        print max_set
+        return len(max_set)
+
+    def longestConsecutiveFast(self, nums):
+        nums_dict = {}
+        res = 0
+        for n in nums:
+            if nums_dict.get(n) is None:
+                left = nums_dict.get(n - 1, 0)
+                right = nums_dict.get(n + 1, 0)
+                sums = left + right + 1
+                nums_dict[n] = sums
+                res = max(res, sums)
+                nums_dict[n - left] = sums
+                nums_dict[n + right] = sums
+        return res
+
+    def longestConsecutive2(self, nums):
+        '''
+        Time limit exceeded
+        :param nums:
+        :return:
+        '''
+        res = 0
+        for n in nums:
+            if not n - 1 in nums:
+                y = n + 1
+                while y in nums:
+                    y += 1
+                res = max(res, y - n)
+        return res
 
 
 solu = Solution()
-solu.longestConsecutive([-9,-4,9,-7,0,7,3,-1,6,2,-2,8,-2,0,2,-7,-5,-2,6,-5,0,-8,8,1,0,6,8,-8,-1])
+print solu.longestConsecutive2([-9,-4,9,-7,0,7,3,-1,6,2,-2,8,-2,0,2,-7,-5,-2,6,-5,0,-8,8,1,0,6,8,-8,-1])
