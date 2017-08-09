a047748a998b4d3aec3bee46ace1635f73c042f5
xilixjd <349729220@qq.com>
2017-08-09 22:57:21 +0800

    练习

--- 练习.py
+++ 练习.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
-# partition 算法
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 import random
 class Solution():
+
+# ==========================快速排序 quicksort===========================================================
     def partition(self, nums, start, end):
         index = random.randint(start, end)
         nums[index], nums[end] = nums[end], nums[index]
@@ -23,7 +29,9 @@ class Solution():
         index = self.partition(nums, start, end)
         self.quickSort(nums, start, index - 1)
         self.quickSort(nums, index + 1, end)
+# =====================================================================================
 
+# ==========================归并排序 mergesort===========================================================
     def mergeSortedArray(self, array_a, array_b):
         array = []
         i = j = 0
@@ -46,9 +54,35 @@ class Solution():
         left = self.mergeSort(array[:mid])
         right = self.mergeSort(array[mid:])
         return self.mergeSortedArray(left, right)
+# =====================================================================================
+
+# ==============================重建二叉树=======================================================
+    def reConstructBinaryTree(self, pre, tin):
+        if len(pre) == 0 or len(tin) == 0:
+            return None
+        inorderMap = {}
+        for i in range(len(tin)):
+            inorderMap[tin[i]] = i
+        return self.construct(0, len(pre) - 1, pre, 0, len(tin) - 1, tin, inorderMap)
+
+    def construct(self, prel, prer, pre, inl, inr, tin, inorderMap):
+        if prel > prer or inl > inr:
+            return
+        root = TreeNode(pre[prel])
+        preIndex = inorderMap[pre[prel]]
+        root.left = self.construct(prel + 1, prel + preIndex - inl, pre, inl, preIndex - 1, tin, inorderMap)
+        root.right = self.construct(prel + preIndex - inl + 1, prer, pre, preIndex + 1, inr, tin, inorderMap)
+        return root
+# =====================================================================================
+
 
 array = [3, 2, 1, 5, 4, 9, 7]
 solu = Solution()
 # print solu.quickSort(array, 0, len(array) - 1)
 # print array
-print solu.mergeSort(array)
+
+# print solu.mergeSort(array)
+
+# pre = [1, 2, 4, 5, 3]
+# tin = [4, 2, 5, 1, 3]
+# print solu.reConstructBinaryTree(pre, tin).left.val
